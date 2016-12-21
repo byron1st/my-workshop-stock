2865bcb851f5e2885485e54430593eea53a4ae4c
byron1st <byron1st@icloud.com>
2016-12-21 18:18:55 +0900

    limit the maximum number of db files to 5

--- src/app/main.js
+++ src/app/main.js
@@ -10,8 +10,10 @@ import * as ch from '../util/ipc.channels'
 const baseDBPathForTest = path.normalize('./test/resource')
 const dbPathForTest = path.join(baseDBPathForTest, 'db')
 const dbPathForProduction = path.join(app.getPath('userData'), 'db')
+const dbFileListLength = 5
 
 let dbPath = ''
+let dbFileList = []
 let mainWindow = null
 let closeConfirmed = false
 
@@ -75,18 +77,22 @@ function prepareTestData () {
 }
 
 function getInitData () {
-  let dbFile = fs.readdirSync(dbPath).reduce((prev, next) => {
-    if (prev > next) {
-      return prev
+  dbFileList = fs.readdirSync(dbPath).sort((prev, next) => {
+    if (prev < next) {
+      return 1
     } else {
-      return next
+      return -1
     }
   })
-  console.log(dbFile)
-  return JSON.parse(fs.readFileSync(path.join(dbPath, dbFile)).toString())
+  return JSON.parse(fs.readFileSync(path.join(dbPath, dbFileList[0])).toString())
 }
 
 function saveDBFile (data) {
   let newDBFilePath = 'db_' + Date.now() + '.json'
+  if (dbFileList.length >= dbFileListLength) {
+    fs.unlinkSync(path.join(dbPath, dbFileList[dbFileListLength - 1]))
+    dbFileList.pop()
+  }
+  dbFileList.unshift(newDBFilePath)
   fs.writeFileSync(path.join(dbPath, newDBFilePath), JSON.stringify(data))
 }

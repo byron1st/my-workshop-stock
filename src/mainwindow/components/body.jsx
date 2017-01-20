837a235ba754160ce0af113ae85330e78593ad41
byron1st <byron1st@icloud.com>
2017-01-20 16:59:53 +0900

    불필요한 파일 및 코멘트 제거

--- src/mainwindow/components/body.jsx
+++ src/mainwindow/components/body.jsx
@@ -1,16 +1,14 @@
 'use strict'
 
-import React, {/*Component, PropTypes*/} from 'react'
+import React from 'react'
 
 import PresentationalComp from './presentational'
-// import BodyTop from './body.top'
 import BodyList from './body.list'
 
 export default class Body extends PresentationalComp {
   render () {
     return (
       <div id='body'>
-        {/*<BodyTop newEvent={this.props.newEvent} productSet={this.props.productSet} productOrder={this.props.productOrder} text={this.props.text}/>*/}
         <BodyList 
           data={this.props.data}
           ui={this.props.ui}

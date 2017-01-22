7c7a716f5f83b1875e1fa94ebeb79b46d88b5dd3
byron1st <byron1st@icloud.com>
2017-01-22 23:09:00 +0900

    util 폴더 내 파일들의 코드 정리

--- src/util/ipc.channels.js
+++ src/util/ipc.channels.js
@@ -1,6 +1,5 @@
 'use strict'
 
-export const SAVE_NEWEVENT = 'ipc-save-newevent'
 export const EXIT = 'ipc-exit'
 export const EXIT_CONFIRMED = 'ipc-exit-confirmed'
 export const BACKUP_DATA = 'ipc-backup-data'

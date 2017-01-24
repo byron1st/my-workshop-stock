b35103dfdede5ecc73283830e7a4178be183e933
byron1st <byron1st@icloud.com>
2017-01-24 14:28:25 +0900

    main 코드 리팩토링 (함수 이름 변경, const 변경)

--- src/main/main.js
+++ src/main/main.js
@@ -17,19 +17,18 @@ const DB_FILE_LIST_LENGTH = 20
 const DB_PATH = _getDBPath(testMode)
 const DB_FILE_LIST = _getDBFileList(DB_PATH)
 
-
 // import os from 'os'
 // const platform = os.platform() + '_' + os.arch()
 // const version = app.getVersion()
 // autoUpdater.setFeedURL('https://lit-bayou-78984.herokuapp.com/update/'+platform+'/'+version)
 
+let locale
 let mainWindow = null
-// let addWindow = null
 let addWindowSet = {}
 let closeConfirmed = false
 
-app.on('ready', initialize)
-app.on('will-quit', wrapUp)
+app.on('ready', _initialize)
+app.on('will-quit', _wrapUp)
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
@@ -37,31 +36,32 @@ app.on('window-all-closed', () => {
 })
 ipcMain.on(ch.EXIT_CONFIRMED, (event, store) => {
   closeConfirmed = true
-  saveDBFile(store)
+  _saveDBFile(store)
   app.quit()
 })
 ipcMain.on(ch.BACKUP_DATA, (event, store) => {
-  saveDBFile(store)
+  _saveDBFile(store)
 })
 ipcMain.on(ch.OPEN_ADDWINDOW, (event, productSet) => {
-  createAddWindow(productSet)
+  _createAddWindow(productSet, locale)
 })
 ipcMain.on(ch.SAVE_EVENTGROUP, (event, eventGroup) => {
   mainWindow.webContents.send(ch.SAVE_EVENTGROUP, eventGroup)
 })
 
-function initialize () {
+function _initialize () {
   // let isUpdate = autoUpdater.checkForUpdates()
   // console.log(isUpdate)
+  locale = _getInitLocale()
   Menu.setApplicationMenu(Menu.buildFromTemplate(menu))
-  createMainWindow(getInitData())
+  _createMainWindow(_getInitData(), locale)
 }
 
-function wrapUp () {
+function _wrapUp () {
   console.log('will quit')
 }
 
-function createMainWindow (initStore) {
+function _createMainWindow (initStore, initLocale) {
   mainWindow = new BrowserWindow({
     width: 995,
     height: 800,
@@ -70,7 +70,7 @@ function createMainWindow (initStore) {
   })
   mainWindow.loadURL(path.join('file://', __dirname, '/../mainwindow/index.html'))
   mainWindow.initStore = initStore
-  mainWindow.initLocale = getInitLocale()
+  mainWindow.initLocale = initLocale
   mainWindow.on('closed', () => {
     mainWindow = null
   })
@@ -87,7 +87,9 @@ function createMainWindow (initStore) {
   }
 }
 
-function createAddWindow (productSet) {
+function _createAddWindow (productSet, initLocale) {
+  const addWindowId = (Date.now()).toString()
+
   let addWindow = new BrowserWindow({
     width: 800,
     height: 450,
@@ -97,11 +99,11 @@ function createAddWindow (productSet) {
       webSecurity: false
     }
   })
-  let addWindowId = (Date.now()).toString()
+
   addWindow.loadURL(path.join('file://', __dirname, '/../addwindow/index.html'))
   addWindow.on('closed', () => delete addWindowSet[addWindowId])
   addWindow.productSet = productSet
-  addWindow.initLocale = getInitLocale()
+  addWindow.initLocale = initLocale
   addWindowSet[addWindowId] = addWindow
 
   if (testMode) {
@@ -110,7 +112,7 @@ function createAddWindow (productSet) {
   }
 }
 
-function getInitData () {
+function _getInitData () {
   const EMPTY_STORE = {
     productSet: {},
     eventSet: {},
@@ -118,8 +120,9 @@ function getInitData () {
     productIdList: [],
     eventGroupIdList: []
   }
+
   if (testMode) {
-    prepareTestData()
+    _prepareTestData()
   } else {
     if (!fs.existsSync(app.getPath('userData'))) {
       return EMPTY_STORE
@@ -128,14 +131,6 @@ function getInitData () {
     }
   }
 
-  // DB_FILE_LIST = fs.readdirSync(DB_PATH).sort((prev, next) => {
-  //   if (prev < next) {
-  //     return 1
-  //   } else {
-  //     return -1
-  //   }
-  // })
-
   if (DB_FILE_LIST.length === 0) {
     // run at first time
     return EMPTY_STORE
@@ -143,23 +138,25 @@ function getInitData () {
   return JSON.parse(fs.readFileSync(path.join(DB_PATH, DB_FILE_LIST[0])).toString())
 }
 
-function prepareTestData () {
-  if (fs.readdirSync(DB_PATH_FOR_TEST).length === 0) {
-    let testDBData = JSON.parse(fs.readFileSync(path.join(BASE_DB_PATH_FOR_TEST, 'db.test.json')).toString())
-    testDBData.eventGroupIdList.forEach(eventGroupId => {
-      testDBData.eventGroupSet[eventGroupId].date = getRandomDate()
-    })
-    testDBData.eventGroupIdList.sort((prev, next) => testDBData.eventGroupSet[next].date - testDBData.eventGroupSet[prev].date)
-    saveDBFile(testDBData)
+function _prepareTestData () {
+  function _getRandomDate () {
+    return new Date(util.getRandomIntInclusive(2013, 2016), util.getRandomIntInclusive(0, 11), util.getRandomIntInclusive(1, 30))
   }
 
-  function getRandomDate () {
-    return new Date(util.getRandomIntInclusive(2013, 2016), util.getRandomIntInclusive(0, 11), util.getRandomIntInclusive(1, 30))
+  if (fs.readdirSync(DB_PATH_FOR_TEST).length === 0) {
+    const testDBData = JSON.parse(fs.readFileSync(path.join(BASE_DB_PATH_FOR_TEST, 'db.test.json')).toString())
+
+    testDBData.eventGroupIdList.forEach(eventGroupId => {
+      testDBData.eventGroupSet[eventGroupId].date = _getRandomDate()
+    })
+    testDBData.eventGroupIdList.sort((prev, next) => testDBData.eventGroupSet[next].date - testDBData.eventGroupSet[prev].date)
+    _saveDBFile(testDBData)
   }
 }
 
-function saveDBFile (data) {
-  let newDBFilePath = 'db_' + Date.now() + '.json'
+function _saveDBFile (data) {
+  const newDBFilePath = 'db_' + Date.now() + '.json'
+
   if (DB_FILE_LIST.length >= DB_FILE_LIST_LENGTH) {
     fs.unlinkSync(path.join(DB_PATH, DB_FILE_LIST[DB_FILE_LIST_LENGTH - 1]))
     DB_FILE_LIST.pop()
@@ -169,10 +166,11 @@ function saveDBFile (data) {
   fs.writeFileSync(path.join(DB_PATH, newDBFilePath), JSON.stringify(data))
 }
 
-function getInitLocale () {
-  let locale = app.getLocale()
+function _getInitLocale () {
+  const locale = app.getLocale()
+
   if (c.LOCALE_LIST.indexOf(locale) === -1) {
-    locale = 'en'
+    return 'en'
   }
   return locale
 }
